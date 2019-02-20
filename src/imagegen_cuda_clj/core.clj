58db14ff11ec319f15d98cf96b92fa98108f54ac
nikonyrh <niko.nyrhila@gmail.com>
2019-02-20 23:21:47 +0200

    tests

--- src/imagegen_cuda_clj/core.clj
+++ src/imagegen_cuda_clj/core.clj
@@ -1,10 +1,12 @@
 (ns imagegen-cuda-clj.core
   (:require [uncomplicate.commons.core :as commons.c]
-            [uncomplicate.clojurecuda.core :as clojurecuda.c]
+            [uncomplicate.clojurecuda.core
+               :as clojurecuda.c
+               :refer [memcpy-host! mem-alloc]]
             [clojure.repl :refer [source doc]]))
 
 
-(defn make-fn [source] ; Must be executed in a CUDA context!
+(defn make-fn! [source] ; Must be executed in a CUDA context!
   (let [name (->> source (re-find #"__global__[ ]+void[ ]*([^ ]+)[ \r\n\t]*\(") second)]
     (-> source
         clojurecuda.c/program
@@ -13,13 +15,11 @@
         (clojurecuda.c/function name))))
 
 
+(defn launch! [kernel-fn grid & parameters] ; Must be executed in a CUDA context!
+  (clojurecuda.c/launch! kernel-fn grid (apply clojurecuda.c/parameters parameters)))
+
+
 (comment
-  (doc clojurecuda.c/program)
-  (doc clojurecuda.c/compile!)
-  (doc clojurecuda.c/module)
-  (doc clojurecuda.c/function)
-  (doc clojurecuda.c/parameters)
-  
   (clojurecuda.c/init)
   (->> (clojurecuda.c/device-count) range (map clojurecuda.c/device) (map commons.c/info) clojure.pprint/pprint)
   
@@ -27,28 +27,28 @@
   
   
   (def sample-source
-    "/* extern \"C\" __device__ float innercall (const float x, const int y) {
-       return x + y;
-     }; */
-     
+    "extern \"C\" __device__ float innercall1 (const float x, const float y, const int i) {
+       return x + y * i;
+     };
+
      extern \"C\" __global__ void example_kernel (const int n, const float *a, float *b, const float c) {
        int i = blockIdx.x * blockDim.x + threadIdx.x;
        if (i < n) {
-         b[i] = a[i] + c; // innercall(a[i], c)
+         b[i] = innercall1(a[i], c, i);
        }
      };")
   
   
   (clojurecuda.c/with-context (clojurecuda.c/context gpu)
-    (let [kernel-fn    (make-fn sample-source)
-          input-buffer (float-array (range n-elems)) ; 0, 1, 2, ...
-          input-gpu    (clojurecuda.c/mem-alloc n-elems)
-          output-gpu   (clojurecuda.c/mem-alloc n-elems)]
+    (let [kernel-fn    (make-fn! sample-source)
+          input-cpu    (float-array (range n-elems)) ; 0, 1, 2, ...
+          input-gpu    (mem-alloc n-elems)
+          output-gpu   (mem-alloc n-elems)]
       
-      (clojurecuda.c/memcpy-host! input-buffer input-gpu)
-      (clojurecuda.c/launch! kernel-fn (clojurecuda.c/grid-1d n-elems) (clojurecuda.c/parameters n-elems input-gpu output-gpu (Float. 1.23)))
+      (memcpy-host! input-cpu input-gpu)
+      (launch! kernel-fn (clojurecuda.c/grid-1d n-elems) n-elems input-gpu output-gpu (Float. 1.23))
       
-      (def output-cpu (clojurecuda.c/memcpy-host! output-gpu (float-array n-elems)))))
+      (def output-cpu (memcpy-host! output-gpu (float-array n-elems)))))
   
   
   (take 10 output-cpu))
