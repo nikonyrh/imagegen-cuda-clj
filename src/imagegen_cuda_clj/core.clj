ab31676b1575afa49e95a3ebd45b00e434433eba
nikonyrh <niko.nyrhila@gmail.com>
2019-02-20 22:46:59 +0200

    Maybe this is a better approach

--- src/imagegen_cuda_clj/core.clj
+++ src/imagegen_cuda_clj/core.clj
@@ -1,53 +1,55 @@
 (ns imagegen-cuda-clj.core
   (:require [uncomplicate.commons.core :as commons.c]
-            [uncomplicate.clojurecuda.core :as clojurecuda.c]))
+            [uncomplicate.clojurecuda.core :as clojurecuda.c]
+            [clojure.repl :refer [source doc]]))
 
 
-; At this point I have no clue if this is a good idea...
-(defn make-kernel-source [name args source]
-  (format "extern \"C\" __global__ void %s (%s) { %s };" name args source))
-
-
-(defn make-fn
-  ([source]
-   (let [name (->> source (re-find #"__global__[ ]+void[ ]*([^ ]+)[ \r\n\t]*\(") second)]
-     (make-fn name source)))
-  ([name source]
-   (-> source
-       clojurecuda.c/program
-       clojurecuda.c/compile!
-       clojurecuda.c/module
-       (clojurecuda.c/function name)))
-  ([name args source]
-   (->> (make-kernel-source name args source)
-        (make-fn name))))
+(defn make-fn [source] ; Must be executed in a CUDA context!
+  (let [name (->> source (re-find #"__global__[ ]+void[ ]*([^ ]+)[ \r\n\t]*\(") second)]
+    (-> source
+        clojurecuda.c/program
+        clojurecuda.c/compile!
+        clojurecuda.c/module
+        (clojurecuda.c/function name))))
 
 
 (comment
+  (doc clojurecuda.c/program)
+  (doc clojurecuda.c/compile!)
+  (doc clojurecuda.c/module)
+  (doc clojurecuda.c/function)
+  (doc clojurecuda.c/parameters)
+  
   (clojurecuda.c/init)
   (->> (clojurecuda.c/device-count) range (map clojurecuda.c/device) (map commons.c/info) clojure.pprint/pprint)
   
-  (def sample-source
-    (make-kernel-source
-      "example_kernel", "const int n, const float *a, float *b"
-      
-      "int i = blockIdx.x * blockDim.x + threadIdx.x;
-       if (i < n) {
-         b[i] = a[i] + 1.0f; // <-- math!
-       }"))
-  
   (def gpu (clojurecuda.c/device 0))
   
+  
+  (def sample-source
+    "/* extern \"C\" __device__ float innercall (const float x, const int y) {
+       return x + y;
+     }; */
+     
+     extern \"C\" __global__ void example_kernel (const int n, const float *a, float *b, const float c) {
+       int i = blockIdx.x * blockDim.x + threadIdx.x;
+       if (i < n) {
+         b[i] = a[i] + c; // innercall(a[i], c)
+       }
+     };")
+  
+  
   (clojurecuda.c/with-context (clojurecuda.c/context gpu)
-    (let [kernel-fn (make-fn sample-source)
+    (let [kernel-fn    (make-fn sample-source)
           input-buffer (float-array (range n-elems)) ; 0, 1, 2, ...
           input-gpu    (clojurecuda.c/mem-alloc n-elems)
-          output-gpu    (clojurecuda.c/mem-alloc n-elems)]
+          output-gpu   (clojurecuda.c/mem-alloc n-elems)]
       
       (clojurecuda.c/memcpy-host! input-buffer input-gpu)
-      (clojurecuda.c/launch! kernel-fn (clojurecuda.c/grid-1d n-elems) (clojurecuda.c/parameters n-elems input-gpu output-gpu))
+      (clojurecuda.c/launch! kernel-fn (clojurecuda.c/grid-1d n-elems) (clojurecuda.c/parameters n-elems input-gpu output-gpu (Float. 1.23)))
       
       (def output-cpu (clojurecuda.c/memcpy-host! output-gpu (float-array n-elems)))))
+  
   
   (take 10 output-cpu))
   
